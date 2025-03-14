5ec9f200f0441c35bad61c950b53c816ce49d962
Cedrix49 <cedrixjames49@gmail.com>
2025-03-14 11:14:13 +0800

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,16 @@
+let scrollTopBtn = document.getElementById('scrollTopBtn');
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollTopBtn.classList.add('show');  
+    } else {
+        scrollTopBtn.classList.remove('show');  
+    }
+};
+
+scrollTopBtn.addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'  
+    });
+});
